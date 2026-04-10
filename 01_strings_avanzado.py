2bedb2b0d904b10ea896dceef2d072195dfc7d87
Jesus Robles <jasasrobles02@gmail.com>
2026-04-09 23:19:57 -0500

    feat(develop): agregando expresiones regulares a temas de ejercicio1

--- 01_strings_avanzado.py
+++ 01_strings_avanzado.py
@@ -1,7 +1,6 @@
 # =============================================================
 # EJERCICIO 1: Strings Avanzado
-# Temas: slicing, métodos de string, palíndromos, anagramas,
-#        cifrado César
+# Temas: slicing, métodos de string, palíndromos, anagramas, expresiones regulares, cifrado César
 # Nivel: básico-intermedio → intermedio
 # =============================================================
 
